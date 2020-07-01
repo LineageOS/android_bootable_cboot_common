6e2367ba7dfcd1347317a233d64737929f6a3389
Aaron Kling <webgeek1234@gmail.com>
2020-07-01 13:03:29 -0500

    Add libavb module
    This is just the rules makefile, the source is referenced from
    the Android tree
    
    Change-Id: I0d76d442f6b6b08a3e902418d0b5902bcea7912d

---
+++ lib/external/libavb/rules.mk
@@ -0,0 +1,31 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+LIBAVB_DIR := $(LOCAL_DIR)/../../../../../../external/avb/libavb
+
+MODULE := $(LOCAL_DIR)
+
+GLOBAL_INCLUDES += $(LOCAL_DIR)/../../../../../../external/avb
+
+MODULE_CFLAGS := -Wno-error=maybe-uninitialized
+
+MODULE_SRCS += \
+        $(LIBAVB_DIR)/avb_chain_partition_descriptor.c \
+        $(LIBAVB_DIR)/avb_cmdline.c \
+        $(LIBAVB_DIR)/avb_crc32.c \
+        $(LIBAVB_DIR)/avb_crypto.c \
+        $(LIBAVB_DIR)/avb_descriptor.c \
+        $(LIBAVB_DIR)/avb_footer.c \
+        $(LIBAVB_DIR)/avb_hash_descriptor.c \
+        $(LIBAVB_DIR)/avb_hashtree_descriptor.c \
+        $(LIBAVB_DIR)/avb_kernel_cmdline_descriptor.c \
+        $(LIBAVB_DIR)/avb_property_descriptor.c \
+        $(LIBAVB_DIR)/avb_rsa.c \
+        $(LIBAVB_DIR)/avb_sha256.c \
+        $(LIBAVB_DIR)/avb_sha512.c \
+        $(LIBAVB_DIR)/avb_slot_verify.c \
+        $(LIBAVB_DIR)/avb_sysdeps_posix.c \
+        $(LIBAVB_DIR)/avb_util.c \
+        $(LIBAVB_DIR)/avb_vbmeta_image.c \
+        $(LIBAVB_DIR)/avb_version.c
+
+include make/module.mk
